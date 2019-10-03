abcd3014862a705ab855aa7ed887a839d75777e2
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-10-03 20:03:22 -0300

    Retirando informações do banco de dados

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const authorization = require('./auth');
-
-//routes
-const authRoutes = require('./routes/auth');
-const usersRoutes = require('./routes/users');
-const siteRoutes = require('./routes/site');
 
 const app = express();
-
-const { Users } = require('./app/models');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -19,14 +11,8 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-const auth = authorization(app);
-app.use(auth.initialize());
-app.auth = auth;
 app.use(cors());
 
-//routes
-authRoutes(app);
-usersRoutes(app, Users);
 siteRoutes(app);
 
 const port = 3000;
