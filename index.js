4bb42e485cdd7e2a5e0f8c2f13527c44ecd32252
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-10-03 19:54:16 -0300

    Adicionando template

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const authorization = require('./auth');
 //routes
 const authRoutes = require('./routes/auth');
 const usersRoutes = require('./routes/users');
+const siteRoutes = require('./routes/site');
 
 const app = express();
 
@@ -13,6 +14,10 @@ const { Users } = require('./app/models');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Permissões do EJS
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 const auth = authorization(app);
 app.use(auth.initialize());
@@ -22,6 +27,7 @@ app.use(cors());
 //routes
 authRoutes(app);
 usersRoutes(app, Users);
+siteRoutes(app);
 
 const port = 3000;
 const server = app.listen(port, () => console.log(`Escutando a porta ${port}!`));
