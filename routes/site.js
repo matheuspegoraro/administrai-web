cb69023891f61f2a0a9126b9e336da67cce1277a
Matheus Pegoraro <matheuspegoraro@icloud.com>
2019-10-22 21:24:43 -0300

    Adicionando telas de login e cadastro

--- routes/site.js
+++ routes/site.js
@@ -2,4 +2,12 @@ module.exports = app => {
     app.get('/', async (req, res) => {
         res.render('../templates/index.ejs'); 
     });
+
+    app.get('/app/register', async (req, res) => {
+        res.render('../templates/register.ejs'); 
+    });
+
+    app.get('/app/login', async (req, res) => {
+        res.render('../templates/login.ejs'); 
+    });
 }
